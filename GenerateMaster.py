2710bc36b169cbed43084b062419c40b5bab04a1
Alexander Trevelyan <atrevely@gmail.com>
2018-04-06 17:01:58 -0700

    Working on concatenating dataframes.

--- GenerateMaster.py
+++ GenerateMaster.py
@@ -15,6 +15,7 @@ def main():
     # more than one, print an error and quit.
     if len(oldMaster) > 1:
         print('Too many master lists supplied! Only room for one (or zero).')
+        print('Shutting down. Please provide fewer master lists.')
         return
 
     # Check to see if we've supplied an existing master list to append to,
@@ -46,6 +47,7 @@ def main():
     # If we didn't find anything new, let us know and quit.
     if filenames == []:
         print('No new files to append!')
+        print('Shutting down. Please add files to append.')
         return
 
     # Read in each new file with Pandas and store them as dictionaries.
@@ -102,34 +104,52 @@ def main():
 
     # Go through each file, grab the new data, and put it in the master list.
     # %%
+    # First, figure out how many rows are currently in the master.
+    currentRows = finalData.shape[0]
+
     # Iterate through each file that we're appending to the master list.
     fileNum = 0
     for filename in filenames:
         # Grab the next file from the list.
         newData = inputData[fileNum]
         fileNum += 1
+        print('Working on file: ' + filename)
 
         # Iterate over each dataframe in the ordered dictionary.
         # Each sheet in the file is its own dataframe in the dictionary.
         for sheetName in list(inputData):
             sheet = inputData[sheetName]
+            totalRows = sheet.shape[0]
+            print('Found ' + str(totalRows) + ' entries in the tab ' + sheetName)
 
             # Iterate over each column of data that we want to append.
             for dataName in list(finalData):
                 # Grab list of names that the data could potentially be under.
                 nameList = lookupTable.at[0, dataName]
 
-
                 # Look for a match in the sheet column names.
                 sheetColumns = list(sheet)
                 columnName = [val for val in sheetColumns if val in nameList]
-                
-                
+
+                # Let us know if we didn't find a column that matches,
+                # or if we found too many columns that match,
+                # otherwise grab the data and put it into the master.
+                if columnName == []:
+                    print('No column found for ' + columnName)
+                elif len(columnName) > 1:
+                    print('Found multiple matches for ' + columnName)
+                    print('Shutting down. Please fix column names.')
+                    return
+                else:
+                    
+                    
+
     # Save the output as a .xlsx file.
     # %%
     writer = pd.ExcelWriter('CurrentMaster.xlsx', engine='xlsxwriter')
     finalData.to_excel(writer, sheet_name='Master')
     writer.save()
+
 
 # Run the main function.
 # %%
