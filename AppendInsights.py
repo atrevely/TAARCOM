e070d211d7b07ed84e8679648ec65ef9536bec23
Alexander Trevelyan <atrevely@gmail.com>
2018-07-25 17:01:25 -0700

    Working on Insights appender.

--- AppendInsights.py
+++ AppendInsights.py
@@ -4,11 +4,13 @@ import os
 
 # The main function.
 def main(filepath):
-    """Docstring.
-    
+    """Appends new Digikey Insight files to the Digikey Insight Master.
+
+    Arguments:
+    filepath -- The filepath to the new Digikey Insight file.
     """
     # Load the Digikey Insights Master file.
-    if os.path.exists('distributorLookup.xlsx'):
+    if os.path.exists('Digikey Insight Master.xlsx'):
         insMast = pd.read_excel('Digikey Insight Master.xlsx',
                                 'Master').fillna('')
         filesProcessed = pd.read_excel('Digikey Insight Master.xlsx',
@@ -20,6 +22,18 @@ def main(filepath):
               '***')
         return
 
+    # Load the Root Customer Mappings file.
+    if os.path.exists('rootCustomerMappings.xlsx'):
+        rootCustMap = pd.read_excel('rootCustomerMappings.xlsx',
+                                    'Sales Lookup').fillna('')
+    else:
+        print('---\n'
+              'No Root Customer Mappings file found!\n'
+              'Please make sure rootCustomerMappings.xlsx'
+              'is in the directory.\n'
+              '***')
+        return
+
     # Strip the root off of the filepath and leave just the filename.
     filename = os.path.basename(filepath)
     if filename in filesProcessed['Filename']:
@@ -28,4 +42,22 @@ def main(filepath):
               'The selected Insight file is already in the Insight Master!\n'
               '***')
         return
+    # Load the Insight file.
+    insFile = pd.read_excel(filepath, None)
+    insFile = insFile[list(insFile)[0]].fillna('')
+
+    # Go through each entry in the Insight file and look for a sales match.
+    for row in range(len(insFile)):
+        salesMatch = insFile.loc[row, 'Root Customer..'] == rootCustMap['Root Customer']
+        match = rootCustMap[salesMatch]
+        if len(match) == 1:
+            # Match to salesperson if exactly one match is found.
+            insFile.loc[row, 'Sales'] = match['Salesperson'].iloc[0]
+            
+        if 'Contract' in insFile[row, 'Root Customer Class']:
+            insFile[row, 'TAARCOM Comments'] = 'Contract Manufacturer'
+        if 'Individual' in insFile[row, 'Root Customer Class']:
+            insFile[row, 'TAARCOM Comments'] = 'Individual'
+            
+    # Check to see if column names match.
 
