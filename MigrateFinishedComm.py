de8be9e295fc3d833fa4781597997bab069f168c
Alexander Trevelyan <atrevely@gmail.com>
2019-03-06 14:37:00 -0800

    Finishing up Commissions Master migration code for testing.

--- MigrateFinishedComm.py
+++ MigrateFinishedComm.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import time
 import datetime
 from dateutil.parser import parse
-import calendar
-import math
 import os.path
 from xlrd import XLRDError
 
@@ -140,9 +137,14 @@ def main(filepath, masterCom):
 
     # Read in the Commissions Master. Exit if not found.
     if os.path.exists('Commissions Master.xlsx'):
-        masterComm = pd.read_excel('Commissions Master.xlsx').fillna('')
+        masterComm = pd.read_excel('Commissions Master.xlsx',
+                                   'Master', dtype=str)
+        masterFiles = pd.read_excel('Commissions Master.xlsx',
+                                    'Files Processed', dtype=str)
         missCols = [i for i in set(masterComm).union(runCom) if
                     i not in list(masterComm) or i not in list(runCom)]
+        masterCols = list(masterComm)
+        fileCols = list(masterFiles)
         if missCols:
             print('The following columns were not detected in one of the two '
                   'files:\n%s' %
@@ -191,20 +193,66 @@ def main(filepath, masterCom):
         # Now match part number.
         partNum = str(runCom.loc[row, 'Part Number']).lower()
         PPN = masterLookup['Part Number'].map(lambda x: str(x).lower())
-        # Reset index, but keep it around for updating usage below.
-        fullMatch = custMatches[partNum == PPN].reset_index()
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        fullMatches = custMatches[PPN == partNum]
+        # Figure out if this entry is a duplicate of any existing entry.
+        for matchID in fullMatches.index:
+            matchCols = ['CM Sales', 'Design Sales', 'CM', 'T-Name',
+                         'T-End Cust']
+            duplicate = all(fullMatches.loc[matchID, i] == runCom.loc[row, i]
+                            for i in matchCols)
+            if duplicate:
+                break
+        # If it's not an exact duplicate, add it to the Lookup Master.
+        if not duplicate:
+            lookupCols = ['CM Sales', 'Design Sales', 'CM', 'T-Name',
+                          'T-End Cust', 'Reported Customer', 'Principal',
+                          'Part Number', 'City']
+            newLookup = runCom.loc[row, lookupCols]
+            newLookup['Date Added'] = datetime.datetime.now().date()
+            newLookup['Last Used'] = datetime.datetime.now().date()
+            masterLookup = masterLookup.append(newLookup, ignore_index=True)
+
+    # Append the new Running Commissions.
+    masterCom = masterCom.append(runCom, ignore_index=True)
+    masterFiles = masterFiles.append(filesProcessed, ignore_index=True)
+    masterCom = masterCom.loc[:, masterCols]
+    masterFiles = masterFiles.loc[:, fileCols]
+
+    # %% Get ready to save files.
+    fname1 = 'Commissions Master.xlsx'
+    fname2 = 'Lookup Master - Current.xlsx'
+
+    if saveError(fname1, fname2):
+        print('---\n'
+              'One or more of these files are currently open in Excel:\n'
+              'Running Commissions, Entries Need Fixing, Lookup Master.\n'
+              'Please close these files and try again.\n'
+              '***')
+        return
+
+    # Write the Commissions Master file.
+    writer1 = pd.ExcelWriter(fname1, engine='xlsxwriter',
+                             datetime_format='mm/dd/yyyy')
+    masterCom.to_excel(writer1, sheet_name='Master', index=False)
+    masterFiles.to_excel(writer1, sheet_name='Files Processed', index=False)
+    # Format everything in Excel.
+    tableFormat(masterCom, 'Master', writer1)
+    tableFormat(masterFiles, 'Files Processed', writer1)
+
+    # Write the Lookup Master.
+    writer2 = pd.ExcelWriter(fname2, engine='xlsxwriter',
+                             datetime_format='mm/dd/yyyy')
+    masterLookup.to_excel(writer2, sheet_name='Lookup', index=False)
+    # Format everything in Excel.
+    tableFormat(masterLookup, 'Lookup', writer2)
+
+    # Save the files.
+    writer1.save()
+    writer2.save()
+
+    print('---\n'
+          'Updates completed successfully!\n'
+          '---\n'
+          'Commissions Master updated.\n'
+          'Lookup Master updated.\n'
+          '+++')
