5140f5947b209b184ea28f9e4285f2c11dae809b
Alexander Trevelyan <atrevely@gmail.com>
2019-11-06 00:06:56 -0800

    Fixed issue with location that part numbers get checked.

--- GenerateMasterGUI.py
+++ GenerateMasterGUI.py
@@ -313,7 +313,7 @@ class GenMast(QMainWindow):
         if self.filenames:
             print('Files selected:')
             for file in self.filenames:
-                print(file)
+                print(os.path.basename(file))
             print('---')
 
     def lockButtons(self):
