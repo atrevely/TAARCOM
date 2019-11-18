00ee47334a85b1f313a2d61cd4b4d80fa04c2caf
Alexander Trevelyan <atrevely@gmail.com>
2019-11-18 00:35:34 -0800

    Changed to correct name when filling in Sales Comm.

--- MergeFixedEntries.py
+++ MergeFixedEntries.py
@@ -164,16 +164,16 @@ def main(runCom):
     # Go through each entry that's fixed and replace it in Running Commissions.
     for row in fixed.index:
         # Fill in the Sales Commission info.
-        salesComm = 0.45*finalData.loc[row, 'Actual Comm Paid']
-        finalData.loc[row, 'Sales Commission'] = salesComm
-        if finalData.loc[row, 'CM Sales']:
+        salesComm = 0.45*fixed.loc[row, 'Actual Comm Paid']
+        fixed.loc[row, 'Sales Commission'] = salesComm
+        if fixed.loc[row, 'CM Sales']:
             # Grab split with default to 20.
-            split = finalData.loc[row, 'CM Split'] or 20
+            split = fixed.loc[row, 'CM Split'] or 20
         else:
             # No CM Sales, so no split.
             split = 0
-        finalData.loc[row, 'CM Sales Comm'] = split*salesComm/100
-        finalData.loc[row, 'Design Sales Comm'] = (100 - split)*salesComm/100
+        fixed.loc[row, 'CM Sales Comm'] = split*salesComm/100
+        fixed.loc[row, 'Design Sales Comm'] = (100 - split)*salesComm/100
         # -------------------------------
         # Make sure the date makes sense.
         # -------------------------------
