58b8e049e00fa00fa253719a06b2e951c2d6b04f
Alexander Trevelyan <atrevely@gmail.com>
2020-10-05 00:37:36 -0700

    City change highlighting fixed.

--- LookupSales.py
+++ LookupSales.py
@@ -51,7 +51,7 @@ def tableFormat(sheetData, sheetName, wbook):
             city_loc = int(np.where(sheetData.columns == 'City on Acct List')[0])
             if not ind and sheetData.loc[row, 'New T-Cust']:
                 sheet.write(row + 1, root_cust_loc, sheetData.loc[row, 'Root Customer..'], newFormat)
-            elif any(sheetData.loc[row, 'City on Acct List']):
+            elif sheetData.loc[row, 'Customer City'] not in sheetData.loc[row, 'City on Acct List'].split(', '):
                 sheet.write(row + 1, root_cust_loc, sheetData.loc[row, 'Root Customer..'],  movedFormat)
                 sheet.write(row + 1, city_loc, sheetData.loc[row, 'City on Acct List'], movedFormat)
     except KeyError:
