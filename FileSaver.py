1785a90414a8cdf86a3deef35bdf3c8fddc24031
Alexander Trevelyan <atrevely@gmail.com>
2024-04-02 10:35:36 -0700

    Initial cleanup and package version updates.

--- FileSaver.py
+++ FileSaver.py
@@ -2,9 +2,8 @@ import pandas as pd
 from RCExcelTools import table_format, save_error
 
 
-def prepare_save_file(filename, tab_data, tab_names):
-    """Prepare a file for saving, returning its writer object."""
-    writer = pd.ExcelWriter(filename, engine='xlsxwriter', datetime_format='mm/dd/yyyy')
+def save_excel_file(filename, tab_data, tab_names):
+    """Save a file as an Excel spreadsheet."""
     if save_error(filename):
         print('---\nThe following file is currently open in Excel:\n %s' % filename
               + '\nPlease close the file and try again.')
@@ -15,18 +14,7 @@ def prepare_save_file(filename, tab_data, tab_names):
         tab_names = [tab_names]
     assert len(tab_data) == len(tab_names), 'Mismatch in size of tab data and tab names.'
     # Add each tab to the document.
-    for data, sheet_name in zip(tab_data, tab_names):
-        data.to_excel(writer, sheet_name=sheet_name, index=False)
-        table_format(sheet_data=data, sheet_name=sheet_name, workbook=writer)
-    return writer
-
-
-def save_files(writer_ojects):
-    """Attempt to save files, aborting if errors are encountered."""
-    if not isinstance(writer_ojects, list):
-        writer_ojects = [writer_ojects]
-    if not all(writer_ojects):
-        return False
-    for writer in writer_ojects:
-        writer.save()
-    return True
+    with pd.ExcelWriter(filename, datetime_format='mm/dd/yyyy') as writer:
+        for data, sheet_name in zip(tab_data, tab_names):
+            data.to_excel(writer, sheet_name=sheet_name, index=False)
+            table_format(sheet_data=data, sheet_name=sheet_name, workbook=writer)
---
+++ GenerateMasterUtils.py
@@ -0,0 +1,86 @@
+import os
+import pandas as pd
+import datetime
+from dateutil.parser import parse
+
+
+def get_column_names(field_mappings):
+    """Generate the commission file column names in the correct order."""
+    # Grab lookup table data names.
+    column_names = list(field_mappings)
+
+    # Add in non-lookup'd data names.
+    column_names[0:0] = ['CM Sales', 'Design Sales']
+    column_names[3:3] = ['T-Name', 'CM', 'T-End Cust']
+    column_names[7:7] = ['Principal', 'Distributor']
+    column_names[18:18] = ['CM Sales Comm', 'Design Sales Comm', 'Sales Commission']
+    column_names[22:22] = ['Quarter Shipped', 'Month', 'Year']
+    column_names.extend(['CM Split', 'Paid Date', 'From File', 'Sales Report Date', 'T-Notes', 'Unique ID'])
+
+    # Make sure that column names are unique.
+    unique_names = []
+    for col_name in column_names:
+        if col_name not in unique_names:
+            unique_names.append(col_name)
+
+    return unique_names
+
+
+def filter_duplicate_files(filepaths, files_processed):
+    """Check to ensure that no duplicate files were provided."""
+    filenames = [os.path.basename(val) for val in filepaths]
+    duplicates = list(set(filenames).intersection(files_processed['Filename']))
+    filenames = [val for val in filenames if val not in duplicates]
+    if duplicates:
+        # Let us know we found duplicates and removed them.
+        print('---\nThe following files are already in Running Commissions:\n%s' %
+              ', '.join(map(str, duplicates)) + '\nDuplicate files were removed from processing.')
+    return filenames
+
+
+def check_for_date_errors(date):
+    # Check if the date is read in as a float/int, and convert to string.
+    if isinstance(date, (float, int)):
+        date = str(int(date))
+    # Check if Pandas read it in as a Timestamp object.
+    # If so, turn it back into a string (a bit roundabout, oh well).
+    elif isinstance(date, (pd.Timestamp, datetime.datetime)):
+        date = str(date)
+    try:
+        parse(date)
+    except (ValueError, TypeError):
+        # The date isn't recognized by the parser.
+        return True
+    except KeyError:
+        print('---\nThere is no Invoice Date column in Running Commissions!\n'
+              'Please check to make sure an Invoice Date column exists.\n'
+              'Note: Spelling, whitespace, and capitalization matter.\n---')
+        return True
+    return False
+
+
+def format_pct_numeric_cols(dataframe):
+    # Convert applicable columns to numeric.
+    numeric_cols = ['Quantity', 'Ext. Cost', 'Invoiced Dollars', 'Paid-On Revenue', 'Actual Comm Paid',
+                    'Unit Cost', 'Unit Price']
+    for col in numeric_cols:
+        try:
+            dataframe[col] = pd.to_numeric(dataframe[col], errors='coerce').fillna('')
+        except KeyError:
+            pass
+
+    # Fix Commission Rate if it got read in as a decimal.
+    pct_cols = ['Commission Rate', 'Split Percentage', 'Gross Rev Reduction', 'Shared Rev Tier Rate']
+    for pct_col in pct_cols:
+        try:
+            # Remove '%' sign if present.
+            col = dataframe[pct_col].astype(str).map(lambda x: x.strip('%'))
+            # Convert to numeric.
+            col = pd.to_numeric(col, errors='coerce')
+            # Identify and correct which entries are not decimal.
+            col[col > 1] /= 100
+            dataframe[pct_col] = col.fillna(0)
+        except (KeyError, TypeError):
+            pass
+
+    return dataframe
